04d0836f26528201ed07cf328604e5bca95f0f01
MiriamCS <al415634@uji.es>
2023-02-28 12:28:51 +0100

    Funciona_v2.0 :D

--- src/main/java/Practica1/CSVTest.java
+++ src/main/java/Practica1/CSVTest.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CSVTest {
-
+    private String[] etiquetas={"sepal length","sepal width","petal length","petal width","class"};
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
     }
@@ -18,7 +18,14 @@ class CSVTest {
     void readTable() throws IOException {
         CSV csv = new CSV();
         Table tabla = csv.readTable("C:\\Users\\al415673\\IdeaProjects\\Practica1\\src\\main\\java\\Practica1\\Iris.txt");
+        //Comprobar que las líneas leidas son correctas
         assertEquals(150, tabla.listaRows.size());
+        //Comprobar que el número de columnas es correcto
         assertEquals(5,tabla.headers.size());
+        //Comprobar que las etiquetas son correctas
+        for(int i=0;i<etiquetas.length;i++){
+            assertEquals(etiquetas[i],tabla.headers.get(i));
+        }
+
     }
 }
