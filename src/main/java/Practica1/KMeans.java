c4e42d5dc88ba0beae7e764494311e31e0feaf3d
al415673 <al415673@uji.es>
2023-03-07 11:04:52 +0100

    Practica 2

---
+++ src/main/java/Practica1/KMeans.java
@@ -0,0 +1,12 @@
+package Practica1;
+
+public class KMeans {
+    //método
+    public void train(Table datos){
+        //Elegir aleatoriamente un semilla
+        double[] vector = new double[4];
+        for(int i=0; i<4; i++){
+            Random random = Random
+        }
+    }
+}
