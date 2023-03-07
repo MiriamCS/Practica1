d319787a37aea27f6dafedc33cadb5cbe7e5617c
al415673 <al415673@uji.es>
2023-03-07 11:58:32 +0100

    Practica 2: Parte 1

--- src/main/java/Practica1/KMeans.java
+++ src/main/java/Practica1/KMeans.java
@@ -1,12 +1,47 @@
 package Practica1;
 
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class KMeans {
-    //método
-    public void train(Table datos){
-        //Elegir aleatoriamente un semilla
-        double[] vector = new double[4];
-        for(int i=0; i<4; i++){
-            Random random = Random
-        }
+    private long seed;
+    private int numIterations;
+    private int numClusters; //numero de grupos
+    //Constructor
+    public KMeans(int numClusters, int numIterations, long seed){
+        this.seed=seed;
+        this.numClusters=numClusters;
+        this.numIterations=numIterations;
     }
+    //método
+    //public void train(TableWithLabels tabla){
+    public void train(Table tabla){
+        //Elegir aleatoriamente un semilla
+        Set<Integer> coleccion = new HashSet<>();
+        int i=0;
+        while (i<numClusters){
+            Random random =new Random(seed);
+            //int aleatorio = random.nextInt(0, tabla.listaRowsLabels.size());
+            int aleatorio = random.nextInt(0, tabla.listaRows.size());
+            if(!coleccion.contains(aleatorio)){
+                System.out.println(aleatorio);
+                coleccion.add(aleatorio);
+                i++;
+                seed++;
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        CSV csv1 = new CSV();
+        TableWithLabels tabla = csv1.readTableLabels("src/main/java/files/Iris");
+        KMeans km1 = new KMeans(3, 5, 415634);
+        km1.train(tabla);
+    }
+
 }
